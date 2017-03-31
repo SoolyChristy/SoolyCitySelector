c7280792fb37bd83ae52e9d8ae1018b0505b73f2
SoolyChristina <402568286@qq.com>
2017-03-31 14:12:48 +0800

    Initial Commit

---
+++ SoolyCitySelector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoolyCitySelector
+//
+//  Created by SoolyChristina on 2017/3/31.
+//  Copyright © 2017年 SoolyChristina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
